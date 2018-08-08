ed5f16735636b0a28e4dfb38ae2f69e10b73c6f6
Vijay Rangan <vjrngn@users.noreply.github.com>
2018-08-08 23:52:10 +0530

    ..

--- __tests__/Exercise1.test.js
+++ __tests__/Exercise1.test.js
@@ -5,9 +5,10 @@ import Button from "../src/exercises/Exercise1";
 test("it must respond to clicks", () => {
   const onClick = jest.fn();
 
-  const { getByText } = render(<Button onClick={onClick} />);
+  const { container, getByText } = render(<Button onClick={onClick} />);
 
   fireEvent.click(getByText("Click Me"));
 
   expect(onClick).toHaveBeenCalledTimes(1);
+  expect(container.firstChild.tagName.toLowerCase()).toEqual("button");
 });
